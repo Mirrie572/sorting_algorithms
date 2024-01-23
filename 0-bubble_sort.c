814ccc9066f5332127ed4f4028a0fcd6aa505c32
Mirrie572 <maggie05072000@gmail.com>
2024-01-23 19:38:19 +0900

    all the O files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,14 +1,13 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
  * bubble_sort - Sorts an array of integers
  * in ascending order using Bubble sort.
+ * 
  * @array: The array to be sorted.
+ * 
  * @size: The size of the array.
  */
-
-void bubble_sort(int *array, size_t size);
 
 void bubble_sort(int *array, size_t size)
 {
