a82ec25c6addc32f2c52aeab58b9adb7e0563265
Mirrie572 <maggie05072000@gmail.com>
2024-01-23 17:11:56 +0900

    task 0 retrial

---
+++ sort.h
@@ -0,0 +1,40 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* all the library to consider for sorting algorithms */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/* ALLOWED,GIVEN STRUCTS TO USE */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* ALL THE PROTOTYPES USED IN THIS PROJECT */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+
+#endif
